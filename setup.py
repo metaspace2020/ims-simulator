cb643ddbcdd9a1137b832de532da71c9049fee70
Theodore Alexandrov <theodorea@gmail.com>
2016-11-30 19:25:33 +0100

    Updated URL in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from ims_simulator import __version__
 
 setup(name='ims_simulator',
       version=__version__,
-      description='Python library for bootstrapping a high resolution imaging dataset',
-      url='https://github.com/spatialmetabolomics/ims_simulator',
+      description='Python library for simulating a high-resolution imaging mass spectrometry data',
+      url='https://github.com/metaspace2020/ims-simulator',
       author='Artem Tarasov, EMBL; Andrew Palmer, EMBL',
       packages=find_packages())
